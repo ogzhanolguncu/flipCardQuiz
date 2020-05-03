b1fa57d2854ba6e1d3e2edb44cfa9c01ec976348
Oğuzhan <ogzhan11@gmail.com>
2020-05-03 20:41:16 +0300

    Updates

---
+++ precache-manifest.3a596b4b67a7d52fde446f9ec11c1ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7830362a315d2deced7ed4d280eff063",
+    "url": "/flipCardQuiz/index.html"
+  },
+  {
+    "revision": "c449f4f3a9fe5bb493a9",
+    "url": "/flipCardQuiz/static/css/main.665880b2.chunk.css"
+  },
+  {
+    "revision": "4f6f5b827f03ff519db9",
+    "url": "/flipCardQuiz/static/js/2.e0ad8e16.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/flipCardQuiz/static/js/2.e0ad8e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c449f4f3a9fe5bb493a9",
+    "url": "/flipCardQuiz/static/js/main.8d735844.chunk.js"
+  },
+  {
+    "revision": "8829c6f92aa3c0a2fa98",
+    "url": "/flipCardQuiz/static/js/runtime-main.27e77a15.js"
+  }
+]);
